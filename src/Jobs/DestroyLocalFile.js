fdc40ff2d27a7df42cde9938b7a6108fdd66c07e
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2026-07-29 16:01:30 +0700

    feat: add destroy local file after 1 month

---
+++ src/Jobs/DestroyLocalFile.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+module.exports = async (channel, storage) => {
+    const processQueue = 'destroyLocalFile';
+    const exchange = 'destroyLocalFileExchange';
+    const waitQueue = 'destroyLocalFileWait';
+    const routingKey = 'destroyLocalFileRouting';
+
+    await channel.assertExchange(exchange, 'direct', { durable: true });
+
+    await channel.assertQueue(processQueue, { durable: true });
+    await channel.bindQueue(processQueue, exchange, routingKey);
+
+    await channel.assertQueue(waitQueue, {
+        durable: true,
+        arguments: {
+            'x-dead-letter-exchange': exchange,
+            'x-dead-letter-routing-key': routingKey,
+            'x-message-ttl': 30 * 24 * 60 * 60 * 1000 // 30 day
+        }
+    });
+
+    channel.consume(processQueue, async (msg) => {
+        if (!msg) return;
+
+        try {
+            const payload = JSON.parse(msg.content.toString());
+            const { filepath } = payload.data;
+
+            const localPath = path.join(process.cwd(), 'Storage', filepath);
+            const fileBuffer = await fs.readFile(localPath);
+
+            storage.fputObject(process.env.S3_OBJECT, filepath, fileBuffer);
+
+            await fs.unlink(localPath);
+
+            channel.ack(msg);
+
+        } catch (error) {
+            console.error('Cannot destroy local file', error.message);
+            channel.nack(msg, false, false);
+        }
+    });
+};
--- src/Jobs/Job.js
+++ src/Jobs/Job.js
@@ -1,5 +1,7 @@
 const documentJob = require('./Document');
+const destroyLocalFile = require('./DestroyLocalFile')
 
 module.exports = async (channel, storage) => {
     documentJob(channel);
+    destroyLocalFile(channel, storage);
 }
