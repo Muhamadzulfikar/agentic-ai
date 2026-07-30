9e17d07d6f2432cae8514f5ad4b7667bd16f5661
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2026-07-30 12:10:21 +0700

    fix: middleware validate machine

--- src/Middlewares/Machine.js
+++ src/Middlewares/Machine.js
@@ -3,21 +3,20 @@ const Database = require('better-sqlite3');
 const db = new Database('agentic-ai.db');
 
 module.exports = (req, res, next) => {
-    const { key, appName, url } = req.headers;
+    const { key, app_name, url } = req.headers;
 
-    const privateKey = key + appName + url;
+    const privateKey = key + app_name + url;
 
     const hashedKey = crypto
         .createHash('sha256')
         .update(privateKey)
         .digest('hex');
 
-    const validate = db.prepare(`SELECT EXISTS
-        (SELECT 1 FROM machines WHERE hashed_key = ?)
-    `).get(hashedKey);
+    const { validate } = db.prepare(`SELECT EXISTS(SELECT 1 FROM machines WHERE hashed_key = ?) as validate`)
+        .get(hashedKey);
 
 
-    if (!validate) {
+    if (! Boolean(validate)) {
         return res.status(401).json({ message: 'API key is not valid' });
     }
 
